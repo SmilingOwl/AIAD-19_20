99eb99cf293b97fdd931ecde82127157667ff172
Helena <helena.montenegro.98@gmail.com>
2019-10-09 12:11:22 +0100

    corrected proposal and added stuff to machines

--- IntelligentFactory/src/agents/Machine.java
+++ IntelligentFactory/src/agents/Machine.java
@@ -5,19 +5,23 @@ import jade.core.Agent;
 import java.util.ArrayList;
 
 public class Machine extends Agent {
-	String id;
+	int id;
 	String role;
-	long average_time;
+	long averageTime;
 	ArrayList<Long> availability; //contains the initial time to perform each task that is already allocated
 	ArrayList<Proposal> proposals;
 	
 	//constructor to initialize machine
-	public Machine() {
-		
+	public Machine(int id, String role, long averageTime) {
+		this.id = id;
+		this.role = role;
+		this.averageTime = averageTime;
 	}
+	
+	
 	
 	//class that starts when the agent is created
 	public void setup() {
-		
+		System.out.println("I'm machine " + this.id);
 	}
 }
