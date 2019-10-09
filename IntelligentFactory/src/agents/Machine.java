84b37a8e19ae8beb8798bc6627caf74e292f356a
Juliana Marques <julianamcm05@gmail.com>
2019-10-09 12:03:59 +0100

    added factory constructor

--- IntelligentFactory/src/agents/Machine.java
+++ IntelligentFactory/src/agents/Machine.java
@@ -1,16 +1,42 @@
 package agents;
 
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.*;
 
 public class Machine extends Agent {
 
-	//constructor to initialize machine
+	private DFAgentDescription dfd;
+	
+	//constructor to initialise machine
 	public Machine() {
+		
 		
 	}
 	
 	//class that starts when the agent is created
 	public void setup() {
 		
+		register();
+	}
+	
+	// register on yellow pages
+	public void register() {
+		ServiceDescription sd = new ServiceDescription();
+		//sd.setType();
+		sd.setName(getLocalName());
+
+		this.dfd = new DFAgentDescription();
+		
+		dfd.setName(getAID());
+		dfd.addServices(sd);
+		
+		try {
+			DFService.register(this, this.dfd);
+		} catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
 	}
 }
+
